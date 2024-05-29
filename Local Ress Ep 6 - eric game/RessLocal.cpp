f9c7ad715e89bff9dd4237c8d1bc55633f3991ce
alvx9 <76928898+alvx9@users.noreply.github.com>
2024-05-29 19:35:55 -0300

    Add files via upload

---
+++ Local Ress Ep 6 - eric game/RessLocal.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+DWORD isNotLocalRessAddr = 0x004D7410;
+DWORD sendRessLocalAddr = 0x004D7351;
+DWORD enable_local_ress_ultimate_mode_return = 0x004D7334;
+_declspec(naked) void enable_local_ress_ultimate_mode() {
+	_asm {
+		test eax,eax
+		je sendRessLocal
+
+		cmp eax,01
+		jne isNotLocalRess
+		jmp enable_local_ress_ultimate_mode_return 
+
+		isNotLocalRess:
+		jmp isNotLocalRessAddr
+
+		sendRessLocal:
+		jmp sendRessLocalAddr
+	}
+}
+
+
+void RessLeaderandLocal() {
+
+	BYTE Enable_Local_Ress[] = { 0x90, 0x90,  0x90,  0x90,  0x90,  0x90 };
+	Hook((PVOID)0x004D732B, enable_local_ress_ultimate_mode, 9);
+}
